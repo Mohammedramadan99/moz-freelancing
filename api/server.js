d5e530a8f37f9d7e014f220d6b0bfb69c34c78be
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-03-05 23:11:14 +0200

    connect to mongodb

---
+++ api/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+const app = express()
+
+// dotenv - to read .env file 
+dotenv.config()
+
+// connect to DB
+mongoose.set('strictQuery',true)
+const connect = async () => {
+
+    try {
+        await mongoose.connect(process.env.DB_URL)
+        console.log('connected to mongodb')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const PORT = process.env.PORT ? process.env.PORT : 8800
+app.listen(PORT,() => {
+    // invocing connect function to connect to the db 
+    connect()
+    console.log(`backend server running ${PORT}`)
+})
